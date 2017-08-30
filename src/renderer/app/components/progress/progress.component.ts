9c83b5ff77bf1718a30dab16a5b23523ea6cd15f
lleohao <lleohao@hotmail.com>
2017-08-30 12:39:21 +0800

    refactor(renderer): chnage progress bar

--- src/renderer/app/components/progress/progress.component.ts
+++ src/renderer/app/components/progress/progress.component.ts
@@ -1,10 +1,18 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
     selector: 'app-progress',
     templateUrl: './progress.component.html',
     styleUrls: ['./progress.component.scss']
 })
-export class ProgressComponent {
+export class ProgressComponent implements OnChanges {
     @Input() progress: number;
+    dasharray: string;
+
+    ngOnChanges() {
+        const percent = this.progress / 100;
+        const perimeter = Math.PI * 2 * 10;
+
+        this.dasharray = perimeter * percent + ' ' + perimeter * (1 - percent);
+    }
 }
